7ed57a18a5568bba27ea2136fb56e54aac16a222
hanlin.huang <280468298@qq.com>
2017-12-26 18:07:13 +0800

    deef

--- examples/tickers/tickers.go
+++ examples/tickers/tickers.go
@@ -1,8 +1,8 @@
 // [Timers](timers) are for when you want to do
 // something once in the future - _tickers_ are for when
-// you want to do something repeatedly at regular
-// intervals. Here's an example of a ticker that ticks
-// periodically until we stop it.
+// you want to do something repeatedly（反复地） at regular（有规律的）
+// intervals（间隔）. Here's an example of a ticker that ticks
+// periodically（定期地） until we stop it.
 
 package main
 
@@ -11,9 +11,9 @@ import "fmt"
 
 func main() {
 
-    // Tickers use a similar mechanism to timers: a
+    // Tickers use a similar（类似的） mechanism（机制） to timers: a
     // channel that is sent values. Here we'll use the
-    // `range` builtin on the channel to iterate over
+    // `range` builtin on（内置在） the channel to iterate over（遍历）
     // the values as they arrive every 500ms.
     ticker := time.NewTicker(time.Millisecond * 500)
     go func() {
