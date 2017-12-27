63aa1873a5b9224edc2ce2122975352c0d3d2f59
hanlin.huang <280468298@qq.com>
2017-12-27 15:38:27 +0800

    deef

--- examples/string-formatting/string-formatting.go
+++ examples/string-formatting/string-formatting.go
@@ -10,99 +10,82 @@ import "os"
 type point struct {
     x, y int
 }
-
 func main() {
 
-    // Go offers several printing "verbs" designed to
-    // format general Go values. For example, this prints
-    // an instance of our `point` struct.
+    // Go提供了几种打印格式，用来格式化一般的Go值，例如
+    // 下面的%v打印了一个point结构体的对象的值
     p := point{1, 2}
     fmt.Printf("%v\n", p)
 
-    // If the value is a struct, the `%+v` variant will
-    // include the struct's field names.
+    // 如果所格式化的值是一个结构体对象，那么`%+v`的格式化输出
+    // 将包括结构体的成员名称和值
     fmt.Printf("%+v\n", p)
 
-    // The `%#v` variant prints a Go syntax representation
-    // of the value, i.e. the source code snippet that
-    // would produce that value.
+    // `%#v`格式化输出将输出一个值的Go语法表示方式。
     fmt.Printf("%#v\n", p)
 
-    // To print the type of a value, use `%T`.
+    // 使用`%T`来输出一个值的数据类型
     fmt.Printf("%T\n", p)
 
-    // Formatting booleans is straight-forward.
+    // 格式化布尔型变量
     fmt.Printf("%t\n", true)
 
-    // There are many options for formatting integers.
-    // Use `%d` for standard, base-10 formatting.
+    // 有很多的方式可以格式化整型，使用`%d`是一种
+    // 标准的以10进制来输出整型的方式
     fmt.Printf("%d\n", 123)
 
-    // This prints a binary representation.
+    // 这种方式输出整型的二进制表示方式
     fmt.Printf("%b\n", 14)
 
-    // This prints the character corresponding to the
-    // given integer.
+    // 这里打印出该整型数值所对应的字符
     fmt.Printf("%c\n", 33)
 
-    // `%x` provides hex encoding.
+    // 使用`%x`输出一个值的16进制表示方式
     fmt.Printf("%x\n", 456)
 
-    // There are also several formatting options for
-    // floats. For basic decimal formatting use `%f`.
+    // 浮点型数值也有几种格式化方法。最基本的一种是`%f`
     fmt.Printf("%f\n", 78.9)
 
-    // `%e` and `%E` format the float in (slightly
-    // different versions of) scientific notation.
+    // `%e`和`%E`使用科学计数法来输出整型
     fmt.Printf("%e\n", 123400000.0)
     fmt.Printf("%E\n", 123400000.0)
 
-    // For basic string printing use `%s`.
+    // 使用`%s`输出基本的字符串
     fmt.Printf("%s\n", "\"string\"")
 
-    // To double-quote strings as in Go source, use `%q`.
+    // 输出像Go源码中那样带双引号的字符串，需使用`%q`
     fmt.Printf("%q\n", "\"string\"")
 
-    // As with integers seen earlier, `%x` renders
-    // the string in base-16, with two output characters
-    // per byte of input.
+    // `%x`以16进制输出字符串，每个字符串的字节用两个字符输出
     fmt.Printf("%x\n", "hex this")
 
-    // To print a representation of a pointer, use `%p`.
+    // 使用`%p`输出一个指针的值
     fmt.Printf("%p\n", &p)
 
-    // When formatting numbers you will often want to
-    // control the width and precision of the resulting
-    // figure. To specify the width of an integer, use a
-    // number after the `%` in the verb. By default the
-    // result will be right-justified and padded with
-    // spaces.
+    // 当输出数字的时候，经常需要去控制输出的宽度和精度。
+    // 可以使用一个位于%后面的数字来控制输出的宽度，默认
+    // 情况下输出是右对齐的，左边加上空格
     fmt.Printf("|%6d|%6d|\n", 12, 345)
 
-    // You can also specify the width of printed floats,
-    // though usually you'll also want to restrict the
-    // decimal precision at the same time with the
-    // width.precision syntax.
+    // 你也可以指定浮点数的输出宽度，同时你还可以指定浮点数
+    // 的输出精度
     fmt.Printf("|%6.2f|%6.2f|\n", 1.2, 3.45)
 
     // To left-justify, use the `-` flag.
     fmt.Printf("|%-6.2f|%-6.2f|\n", 1.2, 3.45)
 
-    // You may also want to control width when formatting
-    // strings, especially to ensure that they align in
-    // table-like output. For basic right-justified width.
+    // 你也可以指定输出字符串的宽度来保证它们输出对齐。默认
+    // 情况下，输出是右对齐的
     fmt.Printf("|%6s|%6s|\n", "foo", "b")
 
-    // To left-justify use the `-` flag as with numbers.
+    // 为了使用左对齐你可以在宽度之前加上`-`号
     fmt.Printf("|%-6s|%-6s|\n", "foo", "b")
 
-    // So far we've seen `Printf`, which prints the
-    // formatted string to `os.Stdout`. `Sprintf` formats
-    // and returns a string without printing it anywhere.
+    // `Printf`函数的输出是输出到命令行`os.Stdout`的，你
+    // 可以用`Sprintf`来将格式化后的字符串赋值给一个变量
     s := fmt.Sprintf("a %s", "string")
     fmt.Println(s)
 
-    // You can format+print to `io.Writers` other than
-    // `os.Stdout` using `Fprintf`.
+    // 你也可以使用`Fprintf`来将格式化后的值输出到`io.Writers`
     fmt.Fprintf(os.Stderr, "an %s\n", "error")
 }
